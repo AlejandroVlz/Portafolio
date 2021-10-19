1914b692cac73ac12fb92fe3e6cd7fe5650d9637
ALejandro Valdez <alexvlzm10@gmail.com>
2021-10-19 14:08:55 -0500

    Inicio del dieño de mi portafolio

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+    var x = document.getElementById("enlaces");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+
+let numeros = [1,2,3];
+
+let numero = numeros.map[1];
+ console.log(numero);
